8463df6da00fa65e7b6a9eb1c633a12adc7f3425
João Paulo Pereira Lima <joaosuporteih@gmail.com>
2025-08-21 22:01:06 -0300

    Adicionado busca por id

--- src/main/java/br/com/jplima/dslist/controllers/GameController.java
+++ src/main/java/br/com/jplima/dslist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.jplima.dslist.dtos.GameDTO;
 import br.com.jplima.dslist.dtos.GameMinDTO;
 import br.com.jplima.dslist.services.GameService;
 
@@ -17,10 +19,17 @@ public class GameController {
 	@Autowired
 	private GameService gameService;
 	
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id){
+		GameDTO resultado = gameService.findById(id);
+		return resultado;
+	}
+	
 	@GetMapping
 	public List<GameMinDTO> findAll(){
-		var result = gameService.findAll();
-		return result;
+		var resultado = gameService.findAll();
+		return resultado;
 	}
+
 
 }
--- src/main/java/br/com/jplima/dslist/services/GameService.java
+++ src/main/java/br/com/jplima/dslist/services/GameService.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import br.com.jplima.dslist.dtos.GameDTO;
 import br.com.jplima.dslist.dtos.GameMinDTO;
 import br.com.jplima.dslist.entities.Game;
 import br.com.jplima.dslist.repostories.GameRepository;
@@ -14,6 +17,14 @@ public class GameService {
 	
 	@Autowired
 	private GameRepository gameRepository;
+	
+	@Transactional(readOnly = true)
+	public GameDTO findById(@PathVariable Long id) {
+		Game resultado = gameRepository.findById(id).get();
+		// criar um tratamento de exceção para depois, para IDs inexistentes
+		GameDTO dto = new GameDTO(resultado);
+		return dto;
+	}
 	
 	public List<GameMinDTO> findAll() {
 		List<Game> resultado = gameRepository.findAll();
